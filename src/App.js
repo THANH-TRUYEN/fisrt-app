061bf1a1b26069a89d5a67e51d6dea9e23a406d1
THANH-TRUYEN <truyenltt.it@gmail.com>
2021-01-21 17:30:33 +0700

    update first-app

--- src/App.js
+++ src/App.js
@@ -3,22 +3,53 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div class="App">
+    <div class="wraper">
+      <div class="left">
+        <img class="logo" src="logo195.jpg"></img>
+        <h3 class= "hello">Hello</h3>
+        <h5 class="name">My name is ....</h5>
+      </div>
+
+      <div class="right">
+        <h1>
+          Introducing Yourself Self     
+        </h1>
+        <h1>
+        Introduction with Text Boxes
+        </h1>
+        <div class="gachdit">
+        </div>
+        <div class="texttren">
+        <div class="text1">
+          <h3> Text1 here 01</h3>
+          <p class= "noidung">5 dieu bac ho day</p>
+          <p class="noidung ">dieu: yeu to quoc yeu dong bao</p>
+        </div>
+        <div class="text2">
+          <h3> Text1 here 02</h3>
+          <p class= "noidung">5 dieu bac ho day</p>
+          <p class="noidung ">dieu: yeu to quoc yeu dong bao</p>
+        </div>
+        </div>
+        <div class="textduoi">
+        <div class="text3">
+          <h3> Text1 here 03</h3>
+          <p class= "noidung">5 dieu bac ho day</p>
+          <p class="noidung ">dieu: yeu to quoc yeu dong bao</p>
+        </div>
+        <div class="text4">
+          <h3> Text1 here 04</h3>
+          <p class= "noidung">5 dieu bac ho day</p>
+          <p class="noidung ">dieu: yeu to quoc yeu dong bao</p>
+        </div>
+        </div>
+      </div>
     </div>
+  </div>
+    
+        
+      
   );
 }
 
